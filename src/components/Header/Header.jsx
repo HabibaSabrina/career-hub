f0f33ddc90bf63d3dbd996a07eaff219a2a1a5b1
HabibaSabrina <habibasabrina92@gmail.com>
2023-04-10 01:10:45 +0600

    navbar style is added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/statistics">Statistics</Link>
-            <Link to="/appliedjobs">Applied jobs</Link>
-            <Link to="/blog">Blog</Link>
+        <div className='my-10 mx-44'>
+            <nav className='flex justify-between items-center'>
+            <h1 className='text-2xl font-bold'>PositionSeeker</h1>
+            <div>
+                <Link className='mx-5'  to="/">Home</Link>
+                <Link className='mx-5' to="/statistics">Statistics</Link>
+                <Link className='mx-5' to="/appliedjobs">Applied jobs</Link>
+                <Link className='mx-5' to="/blog">Blog</Link>
+            </div>
+            <button className='bg-indigo-500 p-4 rounded-xl text-white font-semibold '>Start Applying</button>
+        </nav>
         </div>
     );
 };
