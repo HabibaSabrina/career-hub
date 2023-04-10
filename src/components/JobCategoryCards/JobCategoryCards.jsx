43a50f7f4b9047401585ad5cd3ae643c0eb40b3d
HabibaSabrina <habibasabrina92@gmail.com>
2023-04-10 16:27:08 +0600

    jobs data is created

--- src/components/JobCategoryCards/JobCategoryCards.jsx
+++ src/components/JobCategoryCards/JobCategoryCards.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const JobCategoryCards = ({category}) => {
-    const {image, sector, seats} = category
+    const {categoryName, jobsAvailable, categoryLogo} = category
     return (
         <div className='bg-gray-50 p-10 rounded-xl w-64'>
-            <img className='bg-indigo-50 p-3 mb-5 rounded-xl' src={image} alt="" />
-            <h1 className='my-3 text-xl font-semibold'>{sector}</h1>
-            <p className='text-gray-500'>{seats}+ Jobs Available</p>  
+            <img className='bg-indigo-50 p-3 mb-5 rounded-xl' src={categoryLogo} alt="" />
+            <h1 className='my-3 text-xl font-semibold'>{categoryName}</h1>
+            <p className='text-gray-500'>{jobsAvailable}+ Jobs Available</p>  
         </div>
     );
 };
