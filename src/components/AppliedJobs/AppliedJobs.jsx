de9f296dd307ad12086058b53d5b7636ffaabb4f
HabibaSabrina <habibasabrina92@gmail.com>
2023-04-12 04:28:19 +0600

    filter by is functioning

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getStoredCart } from '../../utilities/fakeDb';
 import { useLoaderData } from 'react-router-dom';
 import AppliedJobsDetails from '../AppliedJobsDetails/AppliedJobsDetails';
-import {ChevronDownIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline'
 
 
 const AppliedJobs = () => {
     const { appliedJobsArr } = useLoaderData()
+    const [filter, setFilter] = useState([...appliedJobsArr])
+    const [open, setOpen] = useState(true)
+
+    const handleFilter = data => {
+        const filterJob = appliedJobsArr.filter(app => app.jobType == data)
+        setFilter([...filterJob])
+        setOpen(!open)
+
+    }
     return (
         <div>
             <h1 className='bg-gray-50 text-center p-20 text-3xl font-bold'>List of Applied Jobs</h1>
-            <div className='flex flex-col gap-5 mx-56 my-20'>
-            {
-                appliedJobsArr.map(appliedJob => <AppliedJobsDetails key={appliedJob.id} appliedJob={appliedJob}></AppliedJobsDetails>)
-            }
+           <div className=' absolute mt-20 right-0 mx-56 bg-gray-50'>
+           <button onClick={() => setOpen(!open)} className=' p-3  w-28 flex justify-between items-center border-2 border-gray-400'>Filter By<span>
+                    {
+                        open === true ? <ChevronDownIcon className="h-5 w-5" /> : <ChevronUpIcon className="h-5 w-5" />
+                    }
+                </span></button>
+            <div>
+                {!open &&
+                    (
+                        <div>
+                            <button className='p-3 w-28 border-x-2 border-b-2 border-gray-400' onClick={() => handleFilter('Onsite')} >Onsite</button>
+                            <br />
+                            <button className='p-3 w-28 border-x-2 border-b-2 border-gray-400' onClick={() => handleFilter('Remote')} >Remote</button>
+                        </div>
+                    )}
+            </div>
+           </div>
+            <div className='flex flex-col gap-5 mx-56 mt-44'>
+                {
+                    filter.map(appliedJob => <AppliedJobsDetails key={appliedJob.id} appliedJob={appliedJob}></AppliedJobsDetails>)
+                }
             </div>
         </div>
     );
---
+++ src/components/SectionBanner/SectionBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SectionBanner = () => {
+    return (
+        <div className=' bg-gray-50 '>
+            <img className='absolute top-36' src="/Vector.png" alt="" />
+            <img className=' absolute overflow-clip right-0 top-0' src="/Vector-1.png" alt="" />
+        </div>
+    );
+};
+
+export default SectionBanner;
