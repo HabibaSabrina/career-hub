5de55a9a6c309e7d701a1d1b2a0a3c581befd32f
HabibaSabrina <habibasabrina92@gmail.com>
2023-04-11 23:42:14 +0600

    See all jobs button is functioning

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Banner from '../Banner/Banner';
 import JobCategory from '../JobCategory/JobCategory';
 import { useLoaderData } from 'react-router-dom';
@@ -7,7 +7,10 @@ import FeaturedJob from '../FeaturedJob/FeaturedJob';
 
 const Home = () => {
     const jobs = useLoaderData()
-    
+    const [seeAll, setSeeAll] = useState(false)
+    const handleSeeAll = () =>{
+        setSeeAll(true)
+    }
     return (
         <div>
             <Banner></Banner>
@@ -18,9 +21,15 @@ const Home = () => {
             </div>
             <div className='grid grid-cols-2 gap-5 mx-52'>
                 {
-                    jobs.map(job => <FeaturedJob key={job.id} job={job}></FeaturedJob>)
+                    jobs?.slice(0, seeAll ? 10 : 4).map(job => <FeaturedJob key={job.id} job={job}></FeaturedJob>)
                 }
             </div>
+            <div className='text-center my-5'>
+                {!seeAll && (
+                <button onClick={handleSeeAll} className=' p-4 rounded-xl mt-5 text-white font-semibold bg-gradient-to-r from-indigo-400 to-violet-600 '>See all jobs</button>
+                )}
+            </div>
+
         </div>
     );
 };
