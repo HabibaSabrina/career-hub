1bc898c9d332ec7bebae21af924eacf18656891e
HabibaSabrina <habibasabrina92@gmail.com>
2023-04-12 05:36:39 +0600

    blog section is added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1>blog</h1>
+            <h1 className='bg-gray-50 text-center p-20 text-3xl font-bold'>Blog</h1>
+            <div className='mx-64 leading-7 my-20'>
+                <p className='text-2xl font-bold mt-5 mb-3'><span className='text-violet-600'>1.</span> When should you use context API?</p>
+                <p className='ml-8'>When I need to access data by many components at different nesting levels. I can avoid to pass props through many levels by <b>context API</b>.</p>
+                <p className='text-2xl font-bold mt-5 mb-3'><span className='text-violet-600'>2.</span> What is a custom hook?</p>
+                <p className='ml-8'><b>Custom hook</b> is a function which has input and output. Its name starts with <b>use</b> and can be used to call other hooks. Custom hook returns a normal non-jsx data and it can use other hooks as well. When we have component logic that needs to be used by multiple components, we can extract that logic to a custom Hook such as <b>useFetch</b>.</p>
+                <p className='text-2xl font-bold mt-5 mb-3'><span className='text-violet-600'>3.</span> What is useRef?</p>
+                <p className='ml-8'><b>useRef()</b> is a built-in React hook that accepts one argument as the initial value and returns a reference. It allows to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</p>
+                <p className='text-2xl font-bold mt-5 mb-3'><span className='text-violet-600'>4.</span> What is useMemo?</p>
+                <p className='ml-8'><b>useMemo()</b> is a function that returns a memoized value of a passed in resource-intensive function.Think of memoization as caching a value so that it does not need to be recalculated. It is very useful in optimizing the performance of a React component by eliminating repeating heavy computations. The useMemo hook is used to optimize the performance of a component by memoizing the results of a calculation or function. It stores the result of a calculation between re-renders.
+                </p>
+            </div>
+
         </div>
     );
 };
