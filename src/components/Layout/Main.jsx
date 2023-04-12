80821011c3f0655faae9f07ed00a1cbe24c250bd
HabibaSabrina <habibasabrina92@gmail.com>
2023-04-12 18:06:48 +0600

    scrolling is fixed

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -3,9 +3,11 @@ import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 import Footer from '../Footer/Footer';
+import ScrollToTop from '../ScrollToTop/ScrollToTop';
 const Main = () => {
     return (
         <div>
+            <ScrollToTop></ScrollToTop>
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
